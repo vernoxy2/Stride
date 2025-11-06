9ab78d43e2a29537073d961db465c26bac056669
vernoxy <vernoxy3@gmail.com>
2025-11-06 12:55:56 +0530

    first commit

--- src/Pages/Home/Sections/Commitment.jsx
+++ src/Pages/Home/Sections/Commitment.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import TitleBox from "../../../Components/TitleBox";
+import icon1 from "../../../assets/HomePage/SVG/Commitment/icon1.svg";
+import icon2 from "../../../assets/HomePage/SVG/Commitment/icon2.svg";
+import icon3 from "../../../assets/HomePage/SVG/Commitment/icon3.svg";
+
+const commitmentItems  = [
+  {
+    id: 1,
+    img: icon1,
+    title: "Corporate Responsibility",
+    desc: "Committed to ethical growth, sustainability, and positive community impact.",
+  },
+  {
+    id: 2,
+    img: icon2,
+    title: "Experts with Team Spirit",
+    desc: "Respect, belonging, fairness, opportunity, understanding, empowerment.",
+  },
+  {
+    id: 3,
+    img: icon3,
+    title: "Diversity, Equity & Inclusion",
+    desc: "Unity through respect, equity, and inclusion.",
+  },
+];
 
 const Commitment = () => {
   return (
     <section>
-      Our Commitment
-    </section>
-  )
-}
+      <div className="container grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
+        {/* 1st column */}
+        <div className="h-full bg-secondary/20"></div>
 
-export default Commitment
+        <div className="space-y-4 p-5">
+          <TitleBox title="Our Commitment" />
+          <h1>Why we stand out</h1>
+          <p>
+            We combine innovation, quality, and passion to deliver unique
+            solutions that stand out, creating lasting value for every customer.
+          </p>
+          <ul>
+            {commitmentItems .map((item) => (
+              <li
+                key={item.id}
+                className="flex flex-col lg:flex-row items-center text-center lg:text-start justify-around gap-2 md:gap-4 py-5 border-t border-[#D9d9d9]"
+              >
+                
+                <img
+                  src={item.img}
+                  alt=""
+                  className="object-cover pointer-events-none"
+                />
+                <p className="text-stride camlecase ml-1 lg:w-[40%] ">{item.title}</p>
+                <p className="text-base md:text-lg lg:text-xl md:w-2/3 ">{item.desc}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Commitment;
--- src/Pages/Home/Sections/Enquiry.jsx
+++ src/Pages/Home/Sections/Enquiry.jsx
@@ -1,20 +1,55 @@
 import React from "react";
 import EnquiryBg from "../../../assets/HomePage/PNG/Enquiry.png";
+import EnquiryBgImg from "../../../assets/HomePage/SVG/EnquiryBgImg.svg";
 import TitleBox from "../../../Components/TitleBox";
 
 const Enquiry = () => {
   return (
     <section
-      className="m-3 rounded-3xl bg-cover bg-center bg-no-repeat relative py-48"
+      className="m-2 md:m-3 rounded-3xl bg-cover bg-center bg-no-repeat relative py-24 md:py-48"
       style={{ backgroundImage: `url(${EnquiryBg})` }}
     >
-      <section className="bg-white w-10/12 py-8 container rounded-xl">
-      <TitleBox title="Quick Enquiry" className="mx-auto"/>
-      <h1 className="text-center">Specialist Property <br /> Support</h1>
+      <section className="bg-white w-11/12 md:w-10/12 py-8 pb-44 container rounded-xl relative z-0">
+        <TitleBox title="Quick Enquiry" className="mx-auto" />
+        <h1 className="text-center">
+          Specialist Property <br /> Support
+        </h1>
+        {/* Form */}
+        <form action="" className="space-y-5 md:space-y-10 xl:w-1/2  mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <input type="text" placeholder="Your Name" className="" required />
+            <input type="email" placeholder="Email" className="" required />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <input
+              type="tel"
+              placeholder="Phone Number"
+              className=""
+              required
+            />
+            <input
+              type="text"
+              placeholder="Your Enquiry About"
+              className=""
+              required
+            />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <p className="md:font-light" >We’re happy to connect! <br /> Required fields are marked</p>
 
+          <button className="w-full bg-secondary hover:bg-stride duration-300 transition-colors rounded-3xl text-white py-1.5 text-lg">
+            Submit
+          </button>
+          </div>
+        </form>
+        {/* Background */}
+        <div className="absolute inset-0 rounded-xl -z-10 flex lg:justify-between justify-end">
+          <img src={EnquiryBgImg} alt="" className="h-full hidden lg:block"/>
+          <img src={EnquiryBgImg} alt="" className="h-full -scale-x-100"/>
+        </div>
       </section>
       {/* Overlay with rounded corners */}
-      <div className="absolute p-12 px-40 bg-white bottom-0 left-0 rounded-se-3xl rounded-es-3xl"></div>
+      <div className="absolute p-6 md:p-12 px-20 md:px-40 bg-white bottom-0 left-0 rounded-se-3xl rounded-es-3xl"></div>
     </section>
   );
 };
