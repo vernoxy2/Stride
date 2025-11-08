a9eca40f1293ae2eb4cf8dedb96caa8add294405
vernoxy <vernoxy3@gmail.com>
2025-11-08 18:01:38 +0530

    Work on About Page

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-// import Footer from "./Sections/Footer";
 import Footer from "../../Components/Footer";
-import FooterData from "../../Components/FooterData";
 import Head from "../../assets/AboutPage/PNG/AboutHead.png"
 import PrimaryHeader from '../../Components/PrimaryHeader';
 import PeopleSay from '../Home/Sections/PeopalSay';
 import Values from './Sections/Values';
 import About from './Sections/About';
-import HorizontalScroll from './Sections/function';
-import HorizontalScrollComponent from './Sections/function';
-import HorizontalScrollContainer from './Sections/function';
-import HorizontalScrollFramer from './Sections/function';
-import HorizontalImageGallery from './Sections/function';
+
 
 const AboutUs = () => {
   return (
@@ -20,7 +14,6 @@ const AboutUs = () => {
       {/* <div className=" px-0 rounded-[50px] md:rounded-[70px] py-10 bg-bg -my-14">
       </div> */}
       <About/>
-      {/* <HorizontalImageGallery/> */}
       <Values/>
       <PeopleSay/>
       <Footer/>
