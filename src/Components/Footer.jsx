48d00e84da593c20e61e1b323b8730a161932b39
vernoxy <vernoxy3@gmail.com>
2025-11-13 15:07:22 +0530

    Work on About Page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = ({ height = "600px", backgroundImage = FooterImg }) => {
   };
 
   return (
-    <section style={style} className="bg-secondary pb-10 before:-z-10 relative before:absolute before:content-[''] before:h-10 before:top-0 before:left-0 before:bg-white overflow-hidden">
+    <section style={style} className="bg-secondary pb-6 md:pb-10 before:-z-10 relative before:absolute before:content-[''] before:h-10 before:top-0 before:left-0 before:bg-white overflow-hidden">
       <h1 data-aos="fade-up" className="text-white text-center container mt-5">
         Where your dream home <br className="hidden md:block" /> becomes reality
       </h1>
